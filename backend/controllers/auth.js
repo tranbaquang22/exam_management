9172cbd95732b150b01b426327f18f6185689eaa
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2025-03-20 09:58:48 +0700

    setup UI and BE

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -21,3 +21,11 @@ export const register = (req, res) => {
         });
     });
 }
+
+export const login = (req, res) => {
+
+};
+
+export const logout = (req, res) => {
+
+};
---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
+    if (!token) return res.status(401).json("Not authenticated!");
+
+    jwt.verify(token, "jwtkey", (err, userInfo) => {
+        if (err) return res.status(403).json("Token is not valid!");
+
+        req.user = userInfo;
+
+        next();
+    });
+};
+
+export const verifyAdmin = (req, res, next) => {
+    if (req.user.role !== 'admin') {
+        return res.status(403).json("Permission denied!");
+    }
+    next();
+};
+
+export const verifyStaff = (req, res, next) => {
+    if (req.user.role === 'staff' || req.user.role === 'admin') next();
+    return res.status(403).json("Permission denied!");
+}
